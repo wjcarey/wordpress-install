01028e57d3b55143cedf6f64a6e64aef258d5022
WJ Carey <wjcarey_personal@pop-os.localdomain>
2020-10-01 18:47:42 -0600

    remove post install file deletion

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 #WORDPRESS INSTALL
-echo "Enter the full path for your wordpress installation ..."
+echo "enter the full path for your wordpress installation ..."
 read INSTALL_PATH
-echo "Confirm  wordpress installation in ${INSTALL_PATH}, Do you want to continue? [Y/n]"
+echo "confirm  wordpress installation in ${INSTALL_PATH}, Do you want to continue? [Y/n]"
 read CONFIRM_INSTALL_LOCATION
 if [ "$CONFIRM_INSTALL_LOCATION" != "${CONFIRM_INSTALL_LOCATION#[Yy]}" ] ;then
     rm ${INSTALL_PATH}/index.html
     wget "https://wordpress.org/latest.zip" -P ${INSTALL_PATH}
     unzip ${INSTALL_PATH}/latest.zip -d ${INSTALL_PATH}
     mv ${INSTALL_PATH}/wordpress/* ${INSTALL_PATH}
-    rm -R ${INSTALL_PATH}/wordpress latest.zip
+    rm -R ${INSTALL_PATH}/wordpress ${INSTALL_PATH}/latest.zip
     chmod 775 -R ${INSTALL_PATH}
     chown -R  www-data:www-data ${INSTALL_PATH}
-    echo "Success: Wordpress installed ..."
+    echo "success: Wordpress installed ..."
 else
-    echo "Notice: the WordPress installer was skipped ..."
+    echo "notice: the wordpress installer was skipped by user..."
 fi
 
 rm -- "$0"
